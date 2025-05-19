3dcfaf35a3ee12a86a0013c2576a687549280f9f
Raulez3104 <raulez3104@hotmail.com>
2025-05-19 19:35:29 -0400

    version 002

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo(){
+    return(
+        <div className="titulo">
+            <h1>Calculadora de IMC</h1>
+            <p>Calculadora de IMC, descubra su peso ideal</p>
+        </div>
+
+    )
+}
+export default Titulo
